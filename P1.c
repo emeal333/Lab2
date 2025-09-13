7ab890ad5a755437a6de339dbc3cd877bf73edb3
Bekah Doody <143531275+bekah-doody@users.noreply.github.com>
2025-09-13 13:11:10 -0400

    Added user input for k & error handling for inputs

--- P1.c
+++ P1.c
@@ -17,20 +17,45 @@ struct Apple {
 int main() {
 
     struct Apple messenger;
-    
+
+
     //Receiving user input
+    int k;
+    printf("Enter number of nodes (int): ");
+    scanf("%d", &k);
+
+    //Validation for k
+    if (k <= 0){
+        printf("Error: Number of nodes must be positive.\n");
+        exit(1);
+    }
+
+    while(getchar() != '\n' && getchar() != EOF);
+
     printf("Enter recipient node (int): ");
     scanf("%d", &messenger.intendedNode);
+
+    if (messenger.intendedNode < 0 || messenger.intendedNode > k){
+        printf("Error: Recipient Node must be between 0 & %d\n", k);
+        exit(1);
+    }
     
     while (getchar() != '\n' && getchar() != EOF);
 
     printf("Enter message (string): ");
     fgets(messenger.message, sizeof(messenger.message), stdin);
+    char *newline = strchr(messenger.message, '\n');
+    if (newline) {
+        *newline = '\0';
+    }
 //
-
+    
+    //TESTING PRINTS REMEMBER TO REMOVE
     printf("Message: %s\n", messenger.message);
     printf("Intended Node: %d\n", messenger.intendedNode);
 
+    
+    
 
     if(signal(SIGINT, interruptSigHandler) == SIG_ERR) {
         perror("signal");
