01426a3a7f779264f1e76e49f5d876e61fae3228
emeal333 <106497339+emeal333@users.noreply.github.com>
2025-09-22 09:41:59 -0400

    Add files via upload
    implementing the while loop to try to make the program reprompt after received a filename

---
+++ LPA4_.c
@@ -0,0 +1,62 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+void* workerThreads(void* arg);
+
+int main () {
+
+    char fileName[200];
+    //printf("Please enter a fileName: ");
+    //scanf("%s", fileName);
+    
+    pthread_t thread1;
+    int threadStatus1;
+    int counter = 0;
+
+    while (counter >= 0) {
+	    printf("Please enter a fileName: ");
+	    scanf("%s", fileName);
+	    
+	    threadStatus1 = pthread_create (&thread1, NULL, workerThreads, &fileName);
+
+	    if (threadStatus1 != 0){
+        fprintf (stderr, "Thread create error %d: %s\n", threadStatus1, strerror(threadStatus1));
+        exit (1);
+    }
+    }
+    //if (threadStatus1 != 0){
+    //    fprintf (stderr, "Thread create error %d: %s\n", threadStatus1, strerror(threadStatus1));
+    //     exit (1);
+    //}
+   
+    
+    
+
+
+    return 0;
+}
+
+void* workerThreads(void* file){
+    char *fileName = (char *)file;
+    int sleepTimes[20] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 8, 9, 10};
+    int randomNumber = (rand() %20); //random number from 0 - 19
+    sleep(sleepTimes[randomNumber]);
+
+    //AWAKEN
+    if (sleepTimes[randomNumber] > 2){
+        printf("File [%s] has not been found\n", fileName);
+    }
+    else {
+        printf("File [%s] has been found\n", fileName);
+    }
+    
+    printf("slept for [%d] second(s)\n", sleepTimes[randomNumber]);
+
+    
+    return NULL;
+}
+
