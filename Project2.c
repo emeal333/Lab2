a91a25d99e368e439ea2f2e2684af0d346928bd2
emeal333 <106497339+emeal333@users.noreply.github.com>
2025-12-02 18:41:50 -0500

    Add files via upload
    added ingredients, and new struct so that ingredients can be kept track of for the bakers

--- Project2.c
+++ Project2.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <semaphore.h>
+#include <string.h>
 
 // #define PERMIT_COUNT 2
 
@@ -14,41 +15,82 @@ sem_t bowl;
 sem_t spoon;
 sem_t oven;
 
-/*
-const char* fridge_ingred[] = {}
-const char* pantry_ingred[] = {}
 
-const int fridge_num_items = ;
-const int pantry_num_items = ;
-*/
+const char* fridge_ingred[] = {"milk", "eggs", "butter"};
+const char* pantry_ingred[] = {"flour", "sugar", "yeast", "baking soda", "salt", "cinnamon"};
+
+const int fridge_num_items = 3;
+const int pantry_num_items = 6;
+
+const char* cookies_ingred[] = {"flour", "sugar", "milk", "butter"};
+const int cookies_num_items = 4;
+const char* pancakes_ingred[] = {"flour", "sugar", "baking soda", "salt", "egg", "milk", "butter"};
+const int pancakes_num_items = 7;
+
+typedef struct {
+	char name[16];
+	int cookies_ingred_taken[4];
+	int pancakes_ingred_taken[7];
+} Baker;
 
 void* thread_callback(void *arg) {
-	char* thread_name = (char*)arg;
-	
+	//char* thread_name = (char*)arg;
+	Baker* baker = (Baker*)arg;
+
 	// fridge cs begins
-	sem_wait(&fridge);    
+	sem_wait(&fridge);
+	printf("%s is in the fridge\n", baker->name);
+
+	//check if baker has items for cookies
+	for (int i = 0; i < fridge_num_items; i++) {
+		for (int j = 0; j < cookies_num_items; j++) {
+			if (strcmp(fridge_ingred[i], cookies_ingred[j]) == 0 && baker->cookies_ingred_taken[j] == 0) {
+				baker->cookies_ingred_taken[j] = 1;
+				printf("%s took %s from fridge\n", baker->name, fridge_ingred[i]);
+			}
+		}
+	}	
 	
-	printf("%s is in the fridge\n", thread_name);
-	
-	printf("%s exiting the fridge\n", thread_name);
+	printf("%s exiting the fridge\n", baker->name);
 	
 	sem_post(&fridge);     
         // fridge cs ends
 	
-	printf("%s exited the fridge\n", thread_name);
+	printf("%s exited the fridge\n", baker->name);
 	
 
-	//pantry cs begins
+	// pantry cs begins
 	sem_wait(&pantry);
+	printf("%s is in the pantry\n", baker->name);
 
-        printf("%s is in the pantry\n", thread_name);
+	// check if baker has items for cookies
+        for (int i = 0; i < pantry_num_items; i++) {
+                for (int j = 0; j < cookies_num_items; j++) {
+                        if (strcmp(pantry_ingred[i], cookies_ingred[j]) == 0 && baker->cookies_ingred_taken[j] == 0) {
+				baker->cookies_ingred_taken[j] = 1;
+				printf("%s took %s from pantry\n", baker->name, pantry_ingred[i]);
+                        }
+                }
+        }
 
-	printf("%s exiting the pantry\n", thread_name);
+	printf("%s exiting the pantry\n", baker->name);
 
         sem_post(&pantry);
-        // pantry cs ends
+        // pantry cs end
 
-        printf("%s exited the pantry\n", thread_name);
+        printf("%s exited the pantry\n", baker->name);
+
+	int complete = 1;
+
+	for (int j = 0; j < cookies_num_items; j++) {
+		if (baker->cookies_ingred_taken[j] == 0) {
+			complete = 0;
+			break;
+		}
+	}
+	if (complete) {
+		printf("%s is ready to mix cookies\n", baker->name);
+	}
 
         return NULL;
 }
@@ -62,28 +104,41 @@ int main() {
 	scanf("%d", &n);
 	
 	pthread_t* threads = malloc(sizeof(pthread_t) * n);
-	char** names = malloc(sizeof(char*) * n);
+	//char** names = malloc(sizeof(char*) * n);
 	
-	sem_init(&fridge, 0, 1); //last input value is how many of that resource there is
+	Baker* bakers = malloc(sizeof(Baker) * n);
+
+	sem_init(&fridge, 0, 2); //last input value is how many of that resource there is
 	sem_init(&pantry, 0, 1); 
 
-	//create n threads
+	//create n threads and bakers(chatGPT helped)
 	for (int i = 0; i < n; i++) {
-		names[i] = malloc(16);
-		sprintf(names[i], "baker%d", i + 1);
+		snprintf(bakers[i].name, sizeof(bakers[i].name), "baker%d", i + 1);
+		memset(bakers[i].cookies_ingred_taken, 0, sizeof(bakers[i].cookies_ingred_taken));
+		memset(bakers[i].pancakes_ingred_taken, 0, sizeof(bakers[i].pancakes_ingred_taken));
 		
-		pthread_create(&threads[i], NULL, thread_callback, names[i]);
+		if (pthread_create(&threads[i], NULL, thread_callback, &bakers[i]) != 0) {
+			perror("pthread_create");
+			// join previously created threads, cleanup
+			for (int k = 0; k < i; k++) pthread_join(threads[k], NULL);
+			sem_destroy(&fridge);
+			sem_destroy(&pantry);
+			free(threads);
+			free(bakers);
+			
+			return 1;
+		}
 	}
 	
 	//join n threads
 	for (int i = 0; i < n; i++) {
 		pthread_join(threads[i], NULL);
-		free(names[i]);
 	}
 	
-	free(names);
-	free(threads);
 	
+	free(threads);
+	free(bakers);
+
 	sem_destroy(&fridge);
 	sem_destroy(&pantry);
 
